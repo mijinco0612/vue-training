83443d7da9e77c9095367230819098048dd7dcd1
Takamichi Tatsumoto <t.t.tatsumoto@gmail.com>
2020-06-10 19:20:01 +0900

    1st step data vind

--- hello/js/main.js
+++ hello/js/main.js
@@ -1,13 +1,6 @@
 var app = new Vue ({
   el:'#app',
   data:{
-    message:'Hello Vue.js!',
-    count:121,
-    user:{
-      lastName:'Yamada',
-      firstName:'Taro',
-      prefecture:'Tokyo'
-    },
-    colors:['Red','Green','Blue']
+    message:'Hello Vue.js!'
   }
 })
