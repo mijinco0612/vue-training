51d4c5d5e621888830bcf5d0af86775e4e656a7d
Takamichi Tatsumoto <TakamichiTatsumoto@gmail.com>
2020-06-15 08:45:19 +0900

    v-on トレーニング

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var app = new Vue({
   el:'#app',
   data:{
-
+    now:''
+  },
+  methods:{
+    onClick:function() {
+      //alert('onclick')
+      this.now = new Date().toLocaleString();
+    }
   }
 })
