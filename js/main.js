972d942457cca25f3987e4a1946c386d15a54679
Takamichi Tatsumoto <TakamichiTatsumoto@gmail.com>
2020-06-15 09:27:08 +0900

    vue summary トレーニング

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var app = new Vue({
   el:'#app',
   data:{
-
+    message:'Hello World!!',
+    url:'https://jp.vuejs.org/',
+    toggle:true,
+    languages:['JavaScript','Ruby','Python']
   }
 })
