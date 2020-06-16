9717ed451ac106bd344d48cdce4f28d19cf99c4c
Takamichi Tatsumoto <TakamichiTatsumoto@gmail.com>
2020-06-16 09:36:02 +0900

    vue v-once

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var app = new Vue({
   el:'#app',
   data:{
-
+    message:'hello vue!'
+  },
+  methods:{
+    clickHandler:function(event) {
+      this.message = this.message.split('').reverse().join('')
+    }
   }
+
 })
