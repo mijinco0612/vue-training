45407b0ea3271b95a392e03b3bc2b1816548f5eb
TakamichiTatsumoto <t.t.tatsumoto@gmail.com>
2020-06-21 02:17:39 +0900

    computed cache

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 var app = new Vue({
   el:'#app',
   data:{
-
+  },
+  computed:{
+    computedNumber:function () {
+      console.log('computed')
+      return Math.random()
+    }
+  },
+  methods :{
+    methodsNumber:function () {
+      console.log('methods')
+      return Math.random()
+    }
   }
+
 })
