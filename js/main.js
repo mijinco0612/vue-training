37f2af65fe84a3bbb258f1c5161b055580ca6f10
TakamichiTatsumoto <t.t.tatsumoto@gmail.com>
2020-06-21 02:02:13 +0900

    computed vs methods

--- js/main.js
+++ js/main.js
@@ -4,7 +4,12 @@ var app = new Vue({
     message:'Hello vue.js'
   },
   computed:{
-    reversedMessage:function () {
+      reversedMessage:function () {
+        return this.message.split('').reverse().join('')
+      }
+    },
+  methods :{
+    reversedMessageMethod :function () {
       return this.message.split('').reverse().join('')
     }
   }
