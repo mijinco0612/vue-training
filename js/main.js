eb32d28a67c9e151ea6d6e8f1fce5bb96379ad05
TakamichiTatsumoto <t.t.tatsumoto@gmail.com>
2020-06-21 01:56:46 +0900

    computed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 var app = new Vue({
   el:'#app',
   data:{
-
+    message:'Hello vue.js'
+  },
+  computed:{
+    reversedMessage:function () {
+      return this.message.split('').reverse().join('')
+    }
   }
 })
