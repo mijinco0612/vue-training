cd80e5503164992f0da828d8a351ae952e988015
TakamichiTatsumoto <t.t.tatsumoto@gmail.com>
2020-06-21 11:33:52 +0900

    watch

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 var app = new Vue({
   el:'#app',
   data:{
-
+    message:'Hello vue.js'
+  },
+  watch:{
+    message: function (newValue,oldValue) {
+      console.log('new:%s,old:%s',newValue,oldValue)
+    }
   }
 })
