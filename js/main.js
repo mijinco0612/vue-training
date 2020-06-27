fbd0ef9ace2bd411a85b25a0967c5829eee8b0c8
TakamichiTatsumoto <t.t.tatsumoto@gmail.com>
2020-06-27 16:34:49 +0900

    deep

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 var app = new Vue({
   el:'#app',
   data:{
+    colors:[
+      {name : 'Red'},
+      {name : 'Green'},
+      {name : 'Bule'}
+    ]
+  },
+  watch:{
+    colors: {
+      handler:function (newValue,oldValue) {
+        console.log('update!')
+        console.log('new:%s,oldValue:%s',
+            JSON.stringify(newValue,null,'\t'),
+        JSON.stringify(oldValue,null,'\t'))
+      },
+      deep:true
+    }
   }
 })
