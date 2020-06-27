21d4c3ce736322f460429191819d4f13c3610dc8
TakamichiTatsumoto <t.t.tatsumoto@gmail.com>
2020-06-27 16:12:01 +0900

    watch vs computed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
 var app = new Vue({
   el:'#app',
   data:{
+    firstName:'',
+    lastName:'',
+  },
+  computed:{
+    fullName : function () {
+      return this.firstName + ' ' + this.lastName
+
+    }
   }
 })
+
+//
+// var app = new Vue({
+//   el:'#app',
+//   data:{
+//     firstName:'',
+//     lastName:'',
+//     fullName:''
+//   },
+//   watch:{
+//     firstName: function (value) {
+//       this.fullName = value + ' ' + this.lastName
+//     },
+//     lastName: function (value) {
+//       this.fullName = this.firstName + ' ' + value
+//     }
+//   }
+//
+//})
